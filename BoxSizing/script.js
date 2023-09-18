92fa9dc574206d5c3ee614fef7c2dc333f6bb42f
yevuno <mari_kola@icloud.com>
2023-09-18 14:46:05 +0200

    Working page & function...

--- BoxSizing/script.js
+++ BoxSizing/script.js
@@ -8,4 +8,5 @@ let displayNumber = document.querySelector(".selectedNumber")
 function changePadding() {
     box.style.padding = paddingSelector.value + "px";
     displayNumber.innerHTML = paddingSelector.value;
+    box.style.boxSizing = document.querySelector("#selectedBox").value;
 }
