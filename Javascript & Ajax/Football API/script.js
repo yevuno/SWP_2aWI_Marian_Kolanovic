8c0ad4ff1d20112698e1e977c40650850556b7c8
yevuno <mari_kola@icloud.com>
2024-02-29 12:14:18 +0100

    Added team logo function

--- Javascript & Ajax/Football API/script.js
+++ Javascript & Ajax/Football API/script.js
@@ -13,7 +13,17 @@ fetch("https://api.openligadb.de/getmatchdata/bl1")
                 let homeTeamText = upcomingMatch.team1.teamName;
                 console.log("Datum:", upcomingMatch.matchDateTime);
 
-                document.getElementById("nextGame").innerHTML = "Nächstes Spiel: " + upcomingMatch.matchDateTime;
+                var matchDate = new Date(upcomingMatch.matchDateTime);
+
+                var year = matchDate.getFullYear();
+                var month = matchDate.getMonth() + 1; 
+                var day = matchDate.getDate();
+                var hours = matchDate.getHours();
+                var minutes = matchDate.getMinutes();
+                
+                var formattedDateTime = day + '.' + month + '.' + year + ' um ' + hours + ':' + (minutes < 10 ? '0' : '') + minutes;
+                
+                document.getElementById("nextGame").innerHTML = formattedDateTime;
 
                 getTeamLogos(guestTeamText, homeTeamText); 
             } else {
@@ -26,13 +36,42 @@ fetch("https://api.openligadb.de/getmatchdata/bl1")
     });
 
     function getTeamLogos(guestTeamText, homeTeamText) {
-        // const homeTeamImageSrc = "https://www.openligadb.de/images/teamicons/" + homeTeamText.replace(/\s/g, "_") + ".png";
-        // const guestTeamImageSrc = "https://www.openligadb.de/images/teamicons/" + guestTeamText.replace(/\s/g, "_") + ".png";
+        const homeTeamLogoSrc = pathToClass[homeTeamText];
+        const guestTeamLogoSrc = pathToClass[guestTeamText];
     
-        // document.getElementById("homeTeam").src = homeTeamImageSrc;
-        // document.getElementById("guestTeam").src = guestTeamImageSrc;
+        if (homeTeamLogoSrc) {
+            document.getElementById("homeTeam").src = homeTeamLogoSrc;
+        } else {
+            console.log("Logo für Heimteam nicht gefunden");
+        }
+    
+        if (guestTeamLogoSrc) {
+            document.getElementById("guestTeam").src = guestTeamLogoSrc;
+        } else {
+            console.log("Logo für Gastteam nicht gefunden");
+        }
+    
         document.getElementById("homeTeamText").innerHTML = homeTeamText;
         document.getElementById("guestTeamText").innerHTML = guestTeamText;
-
     }
     
+
+
+   let pathToClass = {
+    'SC Freiburg': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000A.svg',
+    '1. FC Köln': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-000008.svg',
+    '1. FSV Mainz 05': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-000006.svg',
+    '1. FC Union Berlin': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000V.svg',
+    'Bayer 04 Leverkusen': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000B.svg',
+    'Borussia Dortmund': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000D.svg',
+    'Borussia Mönchengladbach': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000G.svg',
+    'Eintracht Frankfurt': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000E.svg',
+    'FC Augsburg': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000F.svg',
+    'FC Bayern München': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000G.svg',
+    'Hertha BSC': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000H.svg',
+    'RB Leipzig': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000R.svg',
+    'SC Paderborn 07': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000P.svg',
+    'SV Werder Bremen': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000W.svg',
+    'TSG 1899 Hoffenheim': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000H.svg',
+    'VfL Wolfsburg': 'https://www.bundesliga.com/assets/clublogo/DFL-CLU-00000W.svg'
+    };
